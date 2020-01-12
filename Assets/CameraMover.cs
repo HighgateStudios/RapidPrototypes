1b5a37960ff550d1ed543ac29ceb961076f346a2
Jeremy Gallagher <jeremy@highgatestudios.net>
2020-01-12 18:34:12 -0500

    Initial commit hopefully...

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float m_Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * m_Speed * Time.deltaTime;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_ForwardSpeed;
+    public float m_RotationSpeed;
+
+    private GameObject activePipeSection;
+
+    void Update()
+    {
+        MovePlayerForward();
+
+        if (activePipeSection != null)
+        {
+            activePipeSection.transform.RotateAround(transform.position, transform.forward, Input.GetAxis("Horizontal") * m_RotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider pipe)
+    {
+        Debug.Log("Colliding with " + pipe.gameObject.name);
+        activePipeSection = pipe.gameObject;
+    }
+
+    private void MovePlayerForward()
+    {
+        transform.position += transform.forward * m_ForwardSpeed * Time.deltaTime;
+    }
+}
